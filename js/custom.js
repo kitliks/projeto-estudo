94f49126e13256e60a6cd09a1cf198ffa5f8df66
Lucas Vermondes <vermondes@hotmail.com>
2023-09-18 19:13:33 -0300

    localização mapa

--- js/custom.js
+++ js/custom.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
 /** google_map js **/
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(-15.81607534192139, -47.916197593416825),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
